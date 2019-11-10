9a7a2042c5836c59f5ea738f3a0ab5fecfb5fbe4
Tobias Heine <theine@wayfair.com>
2019-11-10 11:35:33 +0100

    Mark data objects as as serializable using kotlinx.serialization

--- data/src/commonMain/kotlin/eu/tobiasheine/movies/data/MovieGallery.kt
+++ data/src/commonMain/kotlin/eu/tobiasheine/movies/data/MovieGallery.kt
@@ -1,7 +1,11 @@
 package eu.tobiasheine.movies.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MovieGallery(val items: List<MovieGalleryItem>)
 
+@Serializable
 data class MovieGalleryItem(
     val movieId: Long,
     val thumbnailUrl: String
