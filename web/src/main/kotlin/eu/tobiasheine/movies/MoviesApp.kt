db2c0ba71696bb7ffa4c84536edaa3530a4568a9
Tobias Heine <theine@wayfair.com>
2020-01-23 14:05:39 +0100

    Consume movies from backend

---
+++ web/src/main/kotlin/eu/tobiasheine/movies/MoviesApp.kt
@@ -0,0 +1,58 @@
+package eu.tobiasheine.movies
+
+import eu.tobiasheine.movies.data.KtorMoviesBackend
+import eu.tobiasheine.movies.data.MovieGallery
+import eu.tobiasheine.movies.data.MoviesBackend
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import react.*
+import react.dom.*
+import kotlin.coroutines.CoroutineContext
+
+class MoviesApp() : RComponent<RProps, MoviesApp.AppState>(), CoroutineScope {
+    private val moviesBackend: MoviesBackend = KtorMoviesBackend()
+    private var job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job
+
+    override fun componentDidMount() {
+        launch {
+            val movieGallery = moviesBackend.movieGallery()
+            setState {
+                this.gallery = movieGallery
+            }
+        }
+    }
+
+    override fun componentWillUnmount() {
+        job.cancel()
+    }
+
+    override fun RBuilder.render() {
+        div {
+            h3 {
+                label {
+                    +"Movies:"
+                }
+                ul {
+                    state.gallery?.items?.forEach { item ->
+                        li {
+                            label {
+                                +item.thumbnailUrl
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    interface AppState : RState {
+        var gallery: MovieGallery?
+    }
+
+}
+
+fun RBuilder.moviesApp() = child(MoviesApp::class) {
+}
--- web/src/main/kotlin/main.kt
+++ web/src/main/kotlin/main.kt
@@ -1,15 +1,9 @@
+import eu.tobiasheine.movies.moviesApp
+import react.dom.render
 import kotlin.browser.document
-import eu.tobiasheine.movies.data.MovieGallery
-import eu.tobiasheine.movies.data.MovieGalleryItem
 
 fun main() {
-    val gallery = MovieGallery(
-        items = listOf(
-            MovieGalleryItem(
-                movieId = 1L,
-                thumbnailUrl = "https://image.tmdb.org/t/p/w1280/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
-            )
-        )
-    )
-    document.write(gallery.toString())
+    render(document.getElementById("app")) {
+        moviesApp()
+    }
 }
